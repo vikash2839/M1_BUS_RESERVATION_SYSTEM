c043e9d833c548ee08abd7f7d20d19854ccbf92d
vikash2839 <kumarvikash.dav@gmail.com>
2022-02-21 16:44:16 +0530

    added information, #1

---
+++ 3_Implementation/src/reservation.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+void bus();
+void booking();
+int read_number(int);
+void read_name(int);
+void status();
+void status_1(int);
+void cancel();
+void helpline();
